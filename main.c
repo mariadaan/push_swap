2793f6b3770ef3c011911b5300a8d60ed2ec4de2
Maria Daan <mariadaan@gmail.com>
2021-11-05 16:54:25 +0100

    tests

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ int	main(int argc, char *argv[])
 	t_stack	stack_b;
 
 	if (check_input(argc, argv + 1))
-		return (error_msg("Error", 1));
+		return (error_msg("Error: non-int arguments", 1));
 	init_stack(&stack_a, argc - 1);
 	init_stack(&stack_b, argc - 1);
 	fill_stack(&stack_a, argv + 1);
 	if (has_duplicates(stack_a.items, stack_a.max_size))
-		return (error_msg("Error", 1));
+		return (error_msg("Error: duplicate numbers", 1));
 	// print_array("stack a", stack_a.items, stack_a.top);
 	// print_array("stack b", stack_b.items, stack_b.top);
 	if (argc == 4)

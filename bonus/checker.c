9a62e56646e0a4af84fe1ef4ca622bcc4425f82a
Maria Daan <mariadaan@gmail.com>
2021-11-23 14:06:20 +0100

    checker norme

--- bonus/checker.c
+++ bonus/checker.c
@@ -1,9 +1,9 @@
 #include "../push_swap.h"
 
-int		is_operation(char *operation)
+int	is_operation(char *operation)
 {
-	char **operations;
-	int	function_index;
+	char	**operations;
+	int		function_index;
 
 	operations = ft_split("sa sb ss pa pb ra rb rr rra rrb rrr", ' ');
 	function_index = ft_strin2darray(operations, operation);
@@ -32,6 +32,7 @@ void	exec_rotate(char *operation, t_stack *a, t_stack *b)
 		rev_rotate(NULL, b);
 	}
 }
+
 void	exec_operation(char *operation, t_stack *a, t_stack *b)
 {
 	if (ft_strcmp(operation, "sa") == 0)
@@ -49,36 +50,23 @@ void	exec_operation(char *operation, t_stack *a, t_stack *b)
 		push(NULL, b, a);
 	else
 		exec_rotate(operation, a, b);
-
-}
-
-int (*p[11]) (char *name, t_stack *a, t_stack *b) = {swap_top, swap_top, swap_top, push, push, rotate, rotate, rotate, rev_rotate, rev_rotate, rev_rotate};
-
-void command(int function_index, t_stack *a, t_stack *b)
-{
-	p[function_index](NULL, a, b);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_stack	a;
 	t_stack	b;
-	char *line;
-	int	function_index;
+	char	*line;
 
 	if (argc == 1)
 		return (1);
 	if (check_input(argc, argv + 1))
 		return (error_msg("Error: non-int arguments", 1));
-	init_stack(&a, argc - 1);
-	init_stack(&b, argc - 1);
-	fill_stack(&a, argv + 1);
-	if (has_duplicates(a.items, a.max_size))
-		return (error_msg("Error: duplicate numbers", 1));
+	if (check_fill_stacks(&a, &b, argv, argc))
+		return (1);
 	while (get_next_line(0, &line))
 	{
-		function_index = is_operation(line);
-		if (function_index != -1)
+		if (is_operation(line) != -1)
 			exec_operation(line, &a, &b);
 		else
 			return (error_msg("Error", 1));

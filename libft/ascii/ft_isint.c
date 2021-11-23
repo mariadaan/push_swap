904c79dd999f530f64d6b3315ba430fe44d611b4
Maria Daan <mariadaan@gmail.com>
2021-11-23 14:03:45 +0100

    norm

--- libft/ascii/ft_isint.c
+++ libft/ascii/ft_isint.c
@@ -5,13 +5,13 @@
 	Returns 1 if num is within integer value boundaries
 	else, returns 0
 */
-int		ft_isint(char *num_str)
+int	ft_isint(char *num_str)
 {
 	int		num_int;
 	char	*new_num_str;
 
 	if (!(num_str[0]))
-			return (1);
+		return (1);
 	num_int = ft_atoi(num_str);
 	new_num_str = ft_itoa(num_int);
 	if (ft_strcmp(num_str, new_num_str))
